932fe021c21132dd80d3269cc7f91419db880a70
shantunu007 <chakmashantunu.web@gmail.com>
2026-07-29 13:39:55 +0600

    Implement get product by ID endpoint

--- index.js
+++ index.js
@@ -53,6 +53,14 @@ async function run() {
             res.send(result);
         });
 
+        // find product
+        app.get("/products/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await productsCollection.findOne(query);
+            res.send(result)
+        });
+
 
         // Define the POST route to receive and save a new user
         app.post("/users", async (req, res) => {
