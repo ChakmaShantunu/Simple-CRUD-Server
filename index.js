0f08b2b3a3a9e420bec42deb966e492bad9a9814
shantunu007 <chakmashantunu.web@gmail.com>
2026-07-30 10:30:54 +0600

    Implement get all categories endpoint

--- index.js
+++ index.js
@@ -48,6 +48,12 @@ async function run() {
             res.send(result);
         });
 
+        app.get("/categories", async (req, res) => {
+            const cursor = categoriesCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
         // find user
         app.get("/users/:id", async (req, res) => {
             const id = req.params.id;
